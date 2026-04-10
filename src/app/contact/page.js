35ba1339e3ac2e6a28ce81ba67f5c01e9b88dac6
learn567 <learne089@gmail.com>
2026-04-10 20:27:19 +0500

    Fixed contact page export

---
+++ src/app/contact/page.js
@@ -0,0 +1,11 @@
+// Example of a correct contact page
+import React from 'react'
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This page is under construction.</p>
+    </div>
+  )
+}
